9a8fdeed6b97afb096d107864c540f6c289a632a
Davis Riedel <mail@davis-riedel.de>
2025-06-02 12:49:37 +0200

    remove doublespending package

--- org.palladiosimulator.blockchainsystems.doublespending/src/main/java/org/palladiosimulator/blockchainsystems/doublespending/behavior/ForkedBlockStorageItem.java
+++
@@ -1,8 +0,0 @@
-package org.palladiosimulator.blockchainsystems.doublespending.behavior;
-
-import org.palladiosimulator.blockchainsystems.core.system.abstractions.Block;
-
-public record ForkedBlockStorageItem(
-        int position,
-        Block block) {
-}
