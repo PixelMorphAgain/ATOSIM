f8a5a4247a0588cb6f6b2f0871e580445cbdfdf7
Davis Riedel <mail@davis-riedel.de>
2025-06-06 22:14:03 +0200

    fix build

--- org.palladiosimulator.blockchainsystems.loggers/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.palladiosimulator.blockchainsystems.loggers {
-    exports org.palladiosimulator.blockchainsystems.loggers;
-
-    requires java.sql;
-    requires transitive org.palladiosimulator.blockchainsystems.core;
-    requires org.postgresql.jdbc;
-}
