d544fd149cce55a8529b9b7cb36e1e625409b8f8
Stephanus Hendarmin <ge63wak@mytum.de>
2026-01-07 17:31:57 +0100

    added block reward monitor, build successful with just

--- org.palladiosimulator.blockchainsystems.threesim/src/main/java/org/palladiosimulator/blockchainsystems/threesim/monitoring/BlockRewardMonitor.java
+++ org.palladiosimulator.blockchainsystems.threesim/src/main/java/org/palladiosimulator/blockchainsystems/threesim/monitoring/BlockRewardMonitor.java
@@ -1,4 +1,35 @@
 package org.palladiosimulator.blockchainsystems.threesim.monitoring;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.palladiosimulator.blockchainsystems.core.block.abstractions.Block;
+
+/**
+ * Tracks block rewards per node.
+ *
+ * Used to compute attacker revenue share.
+ */
 public class BlockRewardMonitor {
+
+    private final Map<String, Integer> rewardsPerNode = new HashMap<>();
+
+    public void recordBlock(Block block) {
+        rewardsPerNode.merge(block.getOriginId(), 1, Integer::sum);
+    }
+
+    public int getTotalRewards() {
+        return rewardsPerNode.values()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public int getRewardsForNode(String nodeId) {
+        return rewardsPerNode.getOrDefault(nodeId, 0);
+    }
+
+    public Map<String, Integer> getAllRewards() {
+        return rewardsPerNode;
+    }
 }
